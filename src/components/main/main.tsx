1b0d3d6538e36178b2afcb2f6ef3e4298008802c
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-04-13 21:30:32 +0200

    assets

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,7 +1,7 @@
-import About from "./main/about";
-import Contact from "./main/contact";
-import Projects from "./main/projects";
-import Skills from "./main/skills";
+import About from './about';
+import Contact from './contact';
+import Projects from './projects';
+import Skills from './skills';
 
 function Main() {
     return (
