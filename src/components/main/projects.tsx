cd0cfb1b927d2d8ed72e32572f58e1f600d7884f
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-06-13 21:16:10 +0200

    added new projects

--- src/components/main/projects.tsx
+++ src/components/main/projects.tsx
@@ -1,31 +1,52 @@
-import bjorkImg from '../assets/projects/bjork.png'
-import ballaUrImg from '../assets/projects/ballaur.png'
-import rapiDapiImg from '../assets/projects/rapidapi.png'
-import quizappImg from '../assets/projects/quizapp.png'
-import calendarImg from '../assets/projects/calendar.png'
-import flightgameImg from '../assets/projects/flightgame.png'
-import '../css/projects.css'
+import bjorkImg from '../assets/projects/bjork.png';
+import ballaUrImg from '../assets/projects/ballaur.png';
+import rapiDapiImg from '../assets/projects/rapidapi.png';
+import quizappImg from '../assets/projects/quizapp.png';
+import calendarImg from '../assets/projects/calendar.png';
+import flightgameImg from '../assets/projects/flightgame.png';
+import rainbowLandImg from '../assets/projects/rainbowLand.png';
+import chatALotImg from '../assets/projects/chatAlot.png';
+import fashionStoreImg from '../assets/projects/fashionStore.png';
+import '../css/projects.css';
 
 interface Project {
-    titel: string;
+    titel?: string;
     src: string;
     alt: string;
-    demoLink: string;
+    demoLink?: string;
     repoLink: string;
 }
 
 function Projects() {
     const projects: Project[] = [
         {
+            // titel: 'Fashion Store',
+            src: fashionStoreImg,
+            alt: 'Fashion Store',
+            // demoLink: '',
+            repoLink: 'https://github.com/feliciavonbraun/fashion-store'
+        },{
+            // titel: 'ChatALot',
+            src: chatALotImg,
+            alt: 'ChatALot',
+            // demoLink: '',
+            repoLink: 'https://github.com/feliciavonbraun/chat-room'
+        },{
+            // titel: 'Rainbow Land',
+            src: rainbowLandImg,
+            alt: 'Rainbow Land',
+            // demoLink: '',
+            repoLink: 'https://github.com/feliciavonbraun/rainbow-land'
+        },{
             titel: 'Björk',
             src: bjorkImg,
-            alt: 'Balla:Ur',
+            alt: 'Björk',
             demoLink: 'https://bjorkinterior.netlify.app/',
             repoLink: 'https://github.com/feliciavonbraun/bjork'
         }, {
             titel: 'Balla:Ur',
             src: ballaUrImg,
-            alt: 'Björk',
+            alt: 'Balla:Ur',
             demoLink: 'https://balla-ur-online.netlify.app/',
             repoLink: 'https://github.com/feliciavonbraun/Ball-game'
         }, {
@@ -64,9 +85,11 @@ function Projects() {
                     <div key={index} className="projectContainer">
                         <img className="computerPicture" src={project.src} alt={project.alt} />
                         <div className="hoverEffects">
-                            <a href={project.demoLink} target="_blank" rel="noreferrer" >
-                                <p className="projectTitle imgZoom">{project.titel} </p>
-                            </a>
+                            {project.titel &&
+                                <a href={project.demoLink} target="_blank" rel="noreferrer" >
+                                    <p className="projectTitle imgZoom">{project.titel} </p>
+                                </a>  
+                            }
                             <a href={project.repoLink} target="_blank" rel="noreferrer">
                                 <p className="projectTitle imgZoom">Repo</p>
                             </a>
