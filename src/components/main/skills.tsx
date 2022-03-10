ea35d969a561a47b6453bb5b661cea8cdf44d218
Felicia von Braun Wallin <feliciavonbraun@hotmail.com>
2022-03-10 22:45:33 +0100

    removed css properties

--- src/components/main/skills.tsx
+++ src/components/main/skills.tsx
@@ -1,116 +1,173 @@
-import htmlLogo from '../../assets/logos/html.png';
-import cssLogo from '../../assets/logos/css.png';
-import jsLogo from '../../assets/logos/js.png';
-import tsLogo from '../../assets/logos/ts-svart.png';
-import reactLogo from '../../assets/logos/react.png';
-import mongoLogo from '../../assets/logos/mongodb3.png';
-import '../css/skills.css';
-// import { CSSProperties } from 'react'
-import { useMediaQuery } from '../mediaQuery';
+import { useMediaQuery } from "../mediaQuery";
+import { Box } from "@material-ui/core";
+import styled from "styled-components";
+
+import htmlLogo from "../../assets/logos/html.png";
+import cssLogo from "../../assets/logos/css.png";
+import jsLogo from "../../assets/logos/js.png";
+import tsLogo from "../../assets/logos/ts-svart.png";
+import reactLogo from "../../assets/logos/react.png";
+import mongoLogo from "../../assets/logos/mongodb3.png";
+// import figmaLogo from "../../assets/logos/figma.png";
+// import styledComponentsLogo from "../../assets/logos/styled-components.png";
 
 interface ImgDetail {
-    img: string;
-    width: string;
-    alt: string;
+  img: string;
+  width: string;
+  alt: string;
 }
 
 function Skills() {
+  const mobileView = useMediaQuery("(max-width: 985px)");
+  // const smallerView = useMediaQuery('(max-width: 550px)');
 
-    let mobileView = useMediaQuery('(max-width: 985px)');
+  const imgDetails: ImgDetail[] = [
+    {
+      img: htmlLogo,
+      // denna ska ju va i % vart ska vi sätta det??
+      width: "100%",
+      alt: "HTML",
+    },
+    {
+      img: cssLogo,
+      // denna ska ju va i % vart ska vi sätta det??
+      width: "85%",
+      alt: "CSS",
+    },
+    {
+      img: jsLogo,
+      // denna ska ju va i % vart ska vi sätta det??
+      width: "100%",
+      alt: "JavaScript",
+    },
+    {
+      img: tsLogo,
+      // denna ska ju va i % vart ska vi sätta det??
+      width: "80%",
+      alt: "TypeScript",
+    },
+    {
+      img: reactLogo,
+      // denna ska ju va i % vart ska vi sätta det??
+      width: "95%",
+      alt: "React",
+    },
+    {
+      img: mongoLogo,
+      // denna ska ju va i % vart ska vi sätta det??
+      width: "45%",
+      alt: "MongoDB",
+    },
+    // {
+    //   img: figmaLogo,
+    //   // denna ska ju va i % vart ska vi sätta det??
+    //   width: "90%",
+    //   alt: "Figma",
+    // },
+    // {
+    //   img: styledComponentsLogo,
+    //   // denna ska ju va i % vart ska vi sätta det??
+    //   width: "80%",
+    //   alt: "styled-components",
+    // },
+  ];
 
-    const imgDetails: ImgDetail[] = [
-        {
-            img: htmlLogo,
-            // denna ska ju va i % vart ska vi sätta det??
-            width: '100%',
-            alt: 'HTML'
-        }, {
-            img: cssLogo,
-            // denna ska ju va i % vart ska vi sätta det??
-            width: '85%',
-            alt: 'CSS'
-        }, {
-            img: jsLogo,
-            // denna ska ju va i % vart ska vi sätta det??
-            width: '100%',
-            alt: 'JavaScript'
-        }, {
-            img: tsLogo,
-            // denna ska ju va i % vart ska vi sätta det??
-            width: '80%',
-            alt: 'TypeScript'
-        }, {
-            img: reactLogo,
-            // denna ska ju va i % vart ska vi sätta det??
-            width: '95%',
-            alt: 'React'
-        }, {
-            img: mongoLogo,
-            // denna ska ju va i % vart ska vi sätta det??
-            width: '45%',
-            alt: 'MongoDB'
-        },
-    ]
+  return (
+    <Box>
+      <Box>
+        <MobileTitle>Skills</MobileTitle>
+        {mobileView && <hr />}
+      </Box>
 
-    return (
-        <div>
-            <div>
-                <h2 className="smallSkillsHeader center">Skills</h2>
-                {mobileView && <hr/> }
-            </div>
+      <SkillsArea id="skills">
+        <DesktopTitle>
+          <TitleLetter>S</TitleLetter>
+          <TitleLetter>K</TitleLetter>
+          <TitleLetter>I</TitleLetter>
+          <TitleLetter>L</TitleLetter>
+          <TitleLetter>L</TitleLetter>
+          <TitleLetter>S</TitleLetter>
+        </DesktopTitle>
 
-            
-            <div id="skills" className="skillsArea flex">
-
-                <div className="bigSkillsHeader flex column">
-                    <h2 className="skillsLetter">S</h2>
-                    <h2 className="skillsLetter">K</h2>
-                    <h2 className="skillsLetter">I</h2>
-                    <h2 className="skillsLetter">L</h2>
-                    <h2 className="skillsLetter">L</h2>
-                    <h2 className="skillsLetter">S</h2>
-                </div>
-
-                {imgDetails.map((imgDetail, index) =>
-                    <div key={index} className="skill">
-                        <img src={imgDetail.img} width={imgDetail.width} alt={imgDetail.alt} />
-                    </div>
-                )}
-            </div>
-        </div>
-    )
+        {/* <ImageGrid> */}
+        {imgDetails.map((imgDetail, index) => (
+          <ImageBox key={index}>
+            <img
+              src={imgDetail.img}
+              width={imgDetail.width}
+              alt={imgDetail.alt}
+            />
+          </ImageBox>
+        ))}
+        {/* </ImageGrid> */}
+      </SkillsArea>
+    </Box>
+  );
 }
 export default Skills;
 
-// fixa conditional rendering på vilken heder som ska visas,
-// ta in mediaquery componenten från chatalot
+const MobileTitle = styled.h2`
+  display: none;
 
-// const skillsArea: CSSProperties = {
-//     padding: '1rem 3rem',
-//     justifyContent: 'space-between',
-//     alignItems: 'center',
-//     width: '100%',
-//     flexWrap: 'wrap',
-// };
+  @media (max-width: 985px) {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 0;
+  }
+`;
 
-// const skillsLetter: CSSProperties = {
-//     margin: 0,
-//     lineHeight: 1,
-// };
+const SkillsArea = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  padding: 1rem 3rem;
+  width: 100%;
+  flex-wrap: wrap;
 
-// const bigSkillsHeader: CSSProperties ={
-//     display: 'block',
-// };
-// const smallSkillsHeader: CSSProperties ={
-//     display: 'none',
-// };
+  @media (max-width: 985px) {
+    padding: 1rem;
+  }
+`;
 
-// const skill: CSSProperties = {
-//     background: 'var(--object-clr)',
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     borderRadius: '.5rem',
-//     width: '7rem',
-//     height: '7rem',
-// };
+const TitleLetter = styled.h2`
+  margin: 0;
+  line-height: 1;
+`;
+
+const DesktopTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 20px;
+
+  @media (max-width: 985px) {
+    display: none;
+  }
+`;
+
+// const ImageGrid = styled.div`
+/* display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(8, 7rem);
+  grid-template-rows: auto auto; */
+// `;
+
+const ImageBox = styled.div`
+  background: var(--object-clr);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.5rem;
+  width: 7rem;
+  height: 7rem;
+
+  @media (max-width: 985px) {
+    width: 15%;
+    margin: 0.1rem;
+    padding: 0.5rem;
+  }
+
+  @media (max-width: 550px) {
+    height: 3rem;
+  }
+`;
